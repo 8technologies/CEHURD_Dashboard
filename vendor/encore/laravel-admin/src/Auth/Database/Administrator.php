49b485211be7051bedd35783ab6f50fb42c3d9e8
Muhindo Mubaraka <30492534+mubahood@users.noreply.github.com>
2023-01-26 17:22:10 +0300

    Update Administrator.php

--- vendor/encore/laravel-admin/src/Auth/Database/Administrator.php
+++ vendor/encore/laravel-admin/src/Auth/Database/Administrator.php
@@ -201,7 +201,6 @@ class Administrator extends Model implements AuthenticatableContract, JWTSubject
         Mail::send('email_view', [], function ($m) {
 
             $m->to("muhindo@8technologies.net", $this->name)
-            ->from('noreply@8technologies.net')
             ->subject('Email Subject!');
         });
 
